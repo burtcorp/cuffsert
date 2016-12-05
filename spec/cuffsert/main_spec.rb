be38cac2f2d604bcc596e9e2a65584ccfbb4a8d0
Quest <quest@lysator.liu.se>
2016-12-05 19:51:27 +0100

    Stack URI is now included in meta

--- spec/cuffsert/main_spec.rb
+++ spec/cuffsert/main_spec.rb
@@ -1,6 +1,34 @@
 require 'spec_helpers'
 require 'tempfile'
 
+describe 'CuffSert#validate_and_urlify' do
+  let(:s3url) { 's3://ze-bucket/some/url' }
+  let(:httpurl) { 'http://some.host/some/file' }
+  
+  it 'urlifies and normalizes files' do
+    stack = Tempfile.new('stack')
+    path = Dir.tmpdir + '/..' + stack.path
+    result = CuffSert.validate_and_urlify(path)
+    expect(result).to eq("file:///#{stack.path}")
+  end
+  
+  it 'respects s3 urls' do
+    expect(CuffSert.validate_and_urlify(s3url)).to eq(s3url)
+  end
+  
+  it 'borks on non-existent local files' do
+    expect { 
+      CuffSert.validate_and_urlify('/no/such/file')
+    }.to raise_error(/local.*not exist/)
+  end
+  
+  it 'borks on unkown schemas' do
+    expect {
+      CuffSert.validate_and_urlize(httpurl)
+    }.to raise_error(/unknown.*http/)
+  end
+end
+
 describe 'CuffSert#build_metadata' do
   subject do
     meta = Tempfile.new('metadata')
