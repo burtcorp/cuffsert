e642412c006269c3e58ef685db7e44b916cf2a71
Quest <quest@lysator.liu.se>
2016-12-11 07:17:59 +0100

    stack_url -> stack_uri

--- lib/cuffsert/main.rb
+++ lib/cuffsert/main.rb
@@ -4,21 +4,24 @@ require 'cuffsert/metadata'
 require 'rx'
 require 'uri'
 
+# TODO:
+# - Stop using file: that we anyway need to special-case in cfarguments
+
 module CuffSert
   def self.validate_and_urlify(stack_path)
     if stack_path =~ /^[A-Za-z0-9]+:/
-      stack_url = URI.parse(stack_path)
+      stack_uri = URI.parse(stack_path)
     else
       normalized = File.expand_path(stack_path)
       unless File.exist?(normalized)
         raise "Local file #{normalized} does not exist"
       end
-      stack_url = URI.join('file:///', normalized)
+      stack_uri = URI.join('file:///', normalized)
     end
-    unless ['s3', 'file'].include?(stack_url.scheme)
-      raise "Uri #{stack_url.scheme} is not supported"
+    unless ['s3', 'file'].include?(stack_uri.scheme)
+      raise "Uri #{stack_uri.scheme} is not supported"
     end
-    stack_url
+    stack_uri
   end
 
   def self.execute(meta, client: RxCFClient.new)
