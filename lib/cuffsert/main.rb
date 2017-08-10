d47e55be57f9f5d5fc0a94d65ab20cfaff7451d7
Quest <quest@lysator.liu.se>
2017-08-10 20:06:56 +0300

    Extract confirmation stuff to separate file

--- lib/cuffsert/main.rb
+++ lib/cuffsert/main.rb
@@ -1,12 +1,12 @@
 require 'cuffsert/cfarguments'
 require 'cuffsert/cfstates'
 require 'cuffsert/cli_args'
+require 'cuffsert/confirmation'
 require 'cuffsert/messages'
 require 'cuffsert/metadata'
 require 'cuffsert/presenters'
 require 'cuffsert/rxcfclient'
 require 'rx'
-require 'termios'
 require 'uri'
 
 # TODO:
@@ -32,47 +32,6 @@ module CuffSert
       raise "Uri #{stack_uri.scheme} is not supported"
     end
     stack_uri
-  end
-
-  def self.need_confirmation(meta, action, desc)
-    return false if meta.op_mode == :dangerous_ok
-    case action
-    when :create
-      false
-    when :update
-      change_set = desc
-      change_set[:changes].any? do |change|
-        rc = change[:resource_change]
-        rc[:action] == 'Remove' || (
-          rc[:action] == 'Modify' &&
-          ['Always', 'True', 'Conditional'].include?(rc[:replacement])
-        )
-      end
-    when :recreate
-      true
-    else
-      true # safety first
-    end
-  end
-
-  def self.ask_confirmation(input = STDIN, output = STDOUT)
-    return false unless input.isatty
-    state = Termios.tcgetattr(input)
-    mystate = state.dup
-    mystate.c_lflag |= Termios::ISIG
-    mystate.c_lflag &= ~Termios::ECHO
-    mystate.c_lflag &= ~Termios::ICANON
-    output.write 'Continue? [yN] '
-    begin
-      Termios.tcsetattr(input, Termios::TCSANOW, mystate)
-      answer = input.getc.chr.downcase
-      output.write("\n")
-      answer == 'y'
-    rescue Interrupt
-      false
-    ensure
-      Termios.tcsetattr(input, Termios::TCSANOW, state)
-    end
   end
 
   def self.create_stack(client, meta, confirm_create)
---
+++ spec/cuffsert/confirmation_spec.rb
@@ -0,0 +1,119 @@
+require 'cuffsert/confirmation'
+require 'spec_helpers'
+require 'stringio'
+
+describe 'CuffSert#need_confirmation' do
+  include_context 'metadata'
+  include_context 'stack states'
+  include_context 'changesets'
+
+  let :local_meta do
+    meta.op_mode = :normal
+    meta
+  end
+
+  subject do
+    CuffSert.need_confirmation(local_meta, :update, change_set_ready)
+  end
+
+  context 'with adds' do
+    let(:change_set_changes) { [r2_add] }
+    it { should be(false) }
+  end
+
+  context 'with non-replace modify' do
+    let(:change_set_changes) { [r1_modify] }
+    it { should be(false) }
+  end
+
+  context 'with conditional replace' do
+    let(:change_set_changes) { [r1_conditional_replace] }
+    it { should be(true) }
+  end
+
+  context 'with known replacement' do
+    let(:change_set_changes) { [r1_replace] }
+    it { should be(true) }
+  end
+
+  context 'with delete' do
+    let(:change_set_changes) { [r3_delete] }
+    it { should be(true) }
+  end
+  
+  context 'given dangerous_ok' do
+    let :local_meta do
+      meta.op_mode = :dangerous_ok
+      meta
+    end
+
+    context 'with known replacement' do
+      let(:change_set_changes) { [r1_replace] }
+      it { should be(false) }
+    end
+
+    context 'with delete' do
+      let(:change_set_changes) { [r3_delete] }
+      it { should be(false) }
+    end
+  end
+  
+  context 'given stack create' do
+    subject { CuffSert.need_confirmation(local_meta, :create, nil) }
+    it { should be(false) }
+  end
+
+  context 'given stack recreate' do
+    subject do
+      CuffSert.need_confirmation(
+        local_meta,
+        :recreate,
+        stack_rolled_back
+      )
+    end
+
+    it { should be(true) }
+
+    context 'with dangerous_ok' do
+      let :local_meta do
+        meta.op_mode = :dangerous_ok
+        meta
+      end
+
+      it { should be(false) }
+    end
+  end
+end
+
+describe 'CuffSert#ask_confirmation' do
+  let(:output) { StringIO.new }
+
+  subject { CuffSert.ask_confirmation(input, output) }
+
+  context 'given non-tty' do
+    let(:input) { StringIO.new }
+
+    it { should be(false) }
+    it { expect(output.string).to eq('') }
+  end
+
+  # context 'given a tty saying yea' do
+  #   let(:input) { double(:stdin, :isatty => true, :getc => 'Y') }
+  #
+  #   it { should be(true) }
+  #   it { expect(output.string).to match(/continue/) }
+  # end
+  #
+  # context 'given a tty saying nay' do
+  #   let(:input) { double(:stdin, :isatty => true, :getc => 'n') }
+  #
+  #   it { should be(false) }
+  #   it { expect(output.string).to match(/continue/) }
+  # end
+  #
+  # context 'given a tty saying foo' do
+  #   let(:input) { double(:stdin, :isatty => true, :getc => 'f') }
+  #
+  #   it { should be(false) }
+  # end
+end
