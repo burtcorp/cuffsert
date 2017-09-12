8324c3d311ef12e89a12c0d2bf1864b291ea01c9
Bittrance <anders.qvist@burtcorp.com>
2017-09-12 23:14:32 +0200

    fixup! Extract common cli args

--- lib/cuffsert/cli_args.rb
+++ lib/cuffsert/cli_args.rb
@@ -1,10 +1,9 @@
 require 'optparse'
+require 'cuff/common_cli_args'
 
 module CuffSert
-  STACKNAME_RE = /^[A-Za-z0-9_-]+$/
-
   def self.parse_cli_args(argv)
-    args = {
+    args = Cuff.default_args.merge!(
       :output => :progressbar,
       :verbosity => 1,
       :force_replace => false,
@@ -13,51 +12,13 @@ module CuffSert
         :parameters => {},
         :tags => {},
       }
-    }
+    )
+
     parser = OptionParser.new do |opts|
       opts.banner = 'Upsert a CloudFormation template, reading creation options and metadata from a yaml file. Currently, parameter values, stack name and stack tags are read from metadata file.'
       opts.separator('')
       opts.separator('Usage: cuffsert --selector production/us stack.json')
-      opts.on('--metadata path', '-m path', 'Yaml file to read stack metadata from') do |path|
-        path = '/dev/stdin' if path == '-'
-        unless File.exist?(path)
-          raise "--metadata #{path} does not exist"
-        end
-        args[:metadata] = path
-      end
-
-      opts.on('--selector selector', '-s selector', 'Dash or slash-separated variant names used to navigate the metadata') do |selector|
-        args[:selector] = selector.split(/[-,\/]/)
-      end
-
-      opts.on('--name stackname', '-n name', 'Alternative stackname (default is to construct the name from the selector') do |stackname|
-        unless stackname =~ STACKNAME_RE
-          raise "--name #{stackname} is expected to be #{STACKNAME_RE.inspect}"
-        end
-        args[:overrides][:stackname] = stackname
-      end
-
-      opts.on('--parameter kv', '-p kv', 'Set the value of a particular parameter, overriding any file metadata') do |kv|
-        key, val = kv.split(/=/, 2)
-        if val.nil?
-          raise "--parameter #{kv} should be key=value"
-        end
-        if args[:overrides][:parameters].include?(key)
-          raise "cli args include duplicate parameter #{key}"
-        end
-        args[:overrides][:parameters][key] = val
-      end
-
-      opts.on('--tag kv', '-t kv', 'Set a stack tag, overriding any file metadata') do |kv|
-        key, val = kv.split(/=/, 2)
-        if val.nil?
-          raise "--tag #{kv} should be key=value"
-        end
-        if args[:overrides][:tags].include?(key)
-          raise "cli args include duplicate tag #{key}"
-        end
-        args[:overrides][:tags][key] = val
-      end
+      Cuff.apply_common_cli_args(args, opts)
 
       opts.on('--json', 'Output events in JSON, no progressbar, colors') do
         args[:output] = :json
@@ -79,17 +40,13 @@ module CuffSert
         raise 'You cannot do --yes and --dry-run at the same time' if args[:op_mode]
         args[:op_mode] = :dangerous_ok
       end
-      
+
       opts.on('--dry-run', 'Describe what would be done') do
         raise 'You cannot do --yes and --dry-run at the same time' if args[:op_mode]
         args[:op_mode] = :dry_run
       end
-
-      opts.on('--help', '-h', 'Produce this message') do
-        abort(opts.to_s)
-      end
     end
-    
+
     args[:stack_path] = parser.parse(argv)
     args
   end
