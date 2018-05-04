burtcorp/cuffsert

commit bc6d0a7c447358262a46bdcbf1e03f0c01523064

Workaround for update not presenting S3 upload messaage. We had a .last in the action which eat the uploading-to-s3 message. Long-term, we should rework all events to be CuffSert::Message objects, but for now, we just move the filtering into flat_map.

lib/cuffsert/rxcfclient.rb
@@ -36,8 +36,10 @@ module CuffSert
         change_set_id = @cf.create_change_set(cfargs)[:id]
         loop do
           change_set = @cf.describe_change_set(change_set_name: change_set_id)
-          observer.on_next(change_set)
-          break if FINAL_STATES.include?(change_set[:status])
+          if FINAL_STATES.include?(change_set[:status])
+            observer.on_next(change_set)
+            break
+          end
         end
         observer.on_completed
       end
@@ -53,7 +55,7 @@ module CuffSert
         observer.on_completed
       end
     end
-    
+
     def abort_update(change_set_id)
       Rx::Observable.create do |observer|
         @cf.delete_change_set(change_set_name: change_set_id)

lib/cuffsert/actions.rb
@@ -51,23 +51,26 @@ module CuffSert
       cfargs[:template_url] = upload_uri if upload_uri
       maybe_upload
         .concat(@cfclient.prepare_update(cfargs))
-        .last
         .flat_map do |change_set|
-          Rx::Observable.concat(
-            Rx::Observable.of(change_set),
-            Rx::Observable.defer {
-              if change_set[:status] == 'FAILED'
-                @cfclient.abort_update(change_set[:change_set_id])
-              elsif @confirmation.call(@meta, :update, change_set)
-                @cfclient.update_stack(change_set[:stack_id], change_set[:change_set_id])
-              else
-                Rx::Observable.concat(
-                  @cfclient.abort_update(change_set[:change_set_id]),
-                  Abort.new('User abort!').as_observable
-                )
-              end
-            }
-          )
+          if change_set.is_a? Aws::CloudFormation::Types::DescribeChangeSetOutput
+            Rx::Observable.concat(
+              Rx::Observable.of(change_set),
+              Rx::Observable.defer {
+                if change_set[:status] == 'FAILED'
+                  @cfclient.abort_update(change_set[:change_set_id])
+                elsif @confirmation.call(@meta, :update, change_set)
+                  @cfclient.update_stack(change_set[:stack_id], change_set[:change_set_id])
+                else
+                  Rx::Observable.concat(
+                    @cfclient.abort_update(change_set[:change_set_id]),
+                    Abort.new('User abort!').as_observable
+                  )
+                end
+              }
+            )
+          else
+            Rx::Observable.just(change_set)
+          end
         end
     end
   end

commit 85049fe647e01c99ddf2326bc56077eeb6bad765

Updates are now executed. Bug introduced in bc6d0a7c which reasonably assumed that the output from #prepare_update was a Aws object while in fact it is a Seahorse response object wrapping the DescribeChangeSetOutput.

lib/cuffsert/presenters.rb
@@ -55,9 +55,6 @@ module CuffSert
     end
 
     def on_event(event)
-      # Workaround for now
-      event = event.data if event.class == Seahorse::Client::Response
-
       case event
       when Aws::CloudFormation::Types::StackEvent
         on_stack_event(event)

lib/cuffsert/rxcfclient.rb
@@ -36,8 +36,8 @@ module CuffSert
         change_set_id = @cf.create_change_set(cfargs)[:id]
         loop do
           change_set = @cf.describe_change_set(change_set_name: change_set_id)
-          if FINAL_STATES.include?(change_set[:status])
-            observer.on_next(change_set)
+          if FINAL_STATES.include?(change_set.data[:status])
+            observer.on_next(change_set.data)
             break
           end
         end

spec/cuffsert/rxcfclient_spec.rb
@@ -110,8 +110,8 @@ describe CuffSert::RxCFClient do
         .at_least(:twice)
         .with(:change_set_name => change_set_id)
         .and_return(
-          change_set_in_progress,
-          change_set_ready
+          double(:seahorse_response, data: change_set_in_progress),
+          double(:seahorse_response, data: change_set_ready)
         )
       expect(subject).to emit_include(change_set_ready)
     end
@@ -120,7 +120,7 @@ describe CuffSert::RxCFClient do
       expect(aws_mock).to receive(:describe_change_set)
         .once
         .with(:change_set_name => change_set_id)
-        .and_return(change_set_failed)
+        .and_return(double(:seahorse_response, data: change_set_failed))
       expect(subject).to emit_include(change_set_failed)
     end
   end